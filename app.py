33966f01ff2eb47bd0c33625d9dee735874da48c
KonchadaBhuvan <konchadabhuvan@gmail.com>
2025-04-12 22:31:20 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request
 from pymongo import MongoClient
-
+import os
 app = Flask(__name__)
 
 # Connect to local MongoDB
 uri = "mongodb+srv://konchadabhuvan22csm:<root>@cluster0.to0pp1g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client = MongoClient(uri)
+client = MongoClient(os.environ.get("MONGODB_URI"))
 db = client["mood_playlist_db"]
 collection = db["playlists"]
 
