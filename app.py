e326b3a5146e17e86f077fe7613210f97bce5a9d
KonchadaBhuvan <konchadabhuvan@gmail.com>
2025-04-12 22:43:19 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -24,4 +24,5 @@ def show_playlist():
     return render_template('playlist.html', mood=mood, songs=songs)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
